e8cff7575910cbe7fab63a1d6d02f205ae0bc414
Grayson Cunningham <gecun289@gmail.com>
2020-11-11 09:38:34 -0700

    Add tests for total_inventory, overstocked_items, and sorted_item_list

--- test/market_test.rb
+++ test/market_test.rb
@@ -19,6 +19,7 @@ class MarketTest < Minitest::Test
     @vendor2.stock(@item3, 25)
     @vendor3 = Vendor.new("Palisade Peach Shack")
     @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
   end
 
   def test_it_exists
@@ -55,5 +56,48 @@ class MarketTest < Minitest::Test
 
     assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
+
+  def test_total_inventory
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = {
+                @item1=> {
+                          quantity: 100,
+                          vendors: [@vendor1, @vendor3]
+                          },
+                @item2 => {
+                          quantity: 7,
+                          vendors: [@vendor1]
+                          },
+                @item4 => {
+                          quantity: 50,
+                          vendors: [@vendor2]
+                          },
+                @item3 => {
+                          quantity: 35,
+                          vendors: [@vendor2, @vendor3]
+                          },
+                }
+    assert_equal expected, @market.total_inventory
+  end
+
+  def test_overstocked_items
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal [@item1], @market.overstocked_items
+  end
+
+  def test_sorted_item_list
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
+    assert_equal expected, @market.sorted_item_list
   end
 end
