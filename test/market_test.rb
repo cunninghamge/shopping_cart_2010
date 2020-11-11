d6d1e8fffb42969302a94ba84f96b1d1e8c858ee
Grayson Cunningham <gecun289@gmail.com>
2020-11-11 09:58:18 -0700

    Add tests for sell method to Market class

--- test/market_test.rb
+++ test/market_test.rb
@@ -1,17 +1,20 @@
 require "minitest/autorun"
 require "minitest/pride"
+require 'mocha/minitest'
 require './lib/item'
 require './lib/vendor'
 require './lib/market'
 
 class MarketTest < Minitest::Test
   def setup
+    Date.stubs(:today).returns(Date.parse("20200224"))
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @item5 = Item.new({name: 'Onion', price: '$0.25'})
     @vendor1.stock(@item1, 35)
     @vendor1.stock(@item2, 7)
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
@@ -116,5 +119,42 @@ class MarketTest < Minitest::Test
 
     expected = ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
     assert_equal expected, @market.sorted_item_list
+  end
+
+  def test_date
+    assert_equal "24/02/2020", @market.date
+  end
+
+  def test_sell_only_returns_true_if_there_are_enough_items
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal false, @market.sell(@item1, 200)
+    assert_equal false, @market.sell(@item5, 1)
+    assert_equal true, @market.sell(@item4, 5)
+  end
+
+  def test_sell_reduces_inventory
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal 50, @vendor2.check_stock(@item4)
+
+    @market.sell(@item4, 5)
+
+    assert_equal 45, @vendor2.check_stock(@item4)
+  end
+
+  def test_sell_can_affect_multiple_vendors
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    @market.sell(@item1, 40)
+
+    assert_equal 0, @vendor1.check_stock(@item1)
+    assert_equal 60, @vendor3.check_stock(@item1)
   end
 end
